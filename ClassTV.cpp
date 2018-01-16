ea5d4d14b5f702624cbd2d47bdc5bf267eb757a6
BahranIlmi <bahranilmi13@gmail.com>
2018-01-16 11:53:44 +0800

    Menambahkan Nilai String

--- ClassTV.cpp
+++ ClassTV.cpp
@@ -11,4 +11,16 @@ class tv{
 int main (){
 	tv lg, sharp, samsung;
 	
+	lg.suara = "Suara lumayan bagus ";
+	sharp.suara = "Suara standar ";
+	samsung.suara = "Suara sangat bagus ";
+	
+	lg.grafik = "Grafik Bagus ";
+	sharp.grafik = "Grafik standar ";
+	samsung.grafik = "Sangat bagus ";
+	
+	lg.ukuran = "Ukuran 40 inch - 49 inch ";
+	sharp.ukuran = "Ukuran 40 inch - 59 inch ";
+	samsung.ukuran = "Ukuran 360 inch - larger ";
+	
 };
